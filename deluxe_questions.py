f5dcd212448f13da2d92db4b11d5ea7fb4c6728c
kiki211 <alexbichevoy211@gmail.com>
2020-06-10 23:37:13 -0700

    21 & pairs functions added

--- deluxe_questions.py
+++ deluxe_questions.py
@@ -5,6 +5,23 @@
 # 3 Anagram
 
 # 4 How many pairs in a string
+one_str = "12345123459087" # 5
+def pairs(one_str):
+    pairs = 0
+    bunker = []
+    for i in one_str:
+        if i in bunker:
+            bunker.remove(i)
+            pairs += 1
+        else:
+            bunker.append(i)
+    return pairs
+
+
+print(f"{pairs(one_str)} pairs.")
+
+
+
 
 
 # 5 return only unique values and the number of unique values
@@ -16,4 +33,12 @@
 
 # 8 Return a string and make letter capital if it follows _ or -.
 
-# 9
+# 9 How many 21 in string?
+my_str = "21asfdsf21221fdsfdsfdsfs21" # answer: 4
+def twenty_1(my_str):
+    return len(my_str.split('21')) - 1
+
+
+print(f"21 repeats {twenty_1(my_str)} times in given string.")
+
+
